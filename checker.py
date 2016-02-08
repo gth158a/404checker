70797e32d9b9241fffdfdd170744ec276ef77034
jaime <jaime.almeida@gatech.edu>
2016-02-07 22:14:31 -0600

    created recursive function

--- checker.py
+++ checker.py
@@ -1,46 +1,70 @@
 import requests
 from urllib.request import urlopen
-from bs4 import BeautifulSoup, SoupStrainer
+from bs4 import BeautifulSoup
 
-site = "https://gradanalytics.georgetown.edu/"
+start = "http://analytics.ncsu.edu/"
+# "http://analytics.ncsu.edu/?page_id=1948"
+# "https://gradanalytics.georgetown.edu/"
 
-try:
-    html = urlopen(site)
-
-except HTTPError as e:
-    print(e)
-# http = httplib2.Http()
-# status, response = http.request('http://www.nytimes.com')
-#
+pages = set()  # pages collected in current site
+pagesError = set()  # url of pages with issues
+visitedPages = set()  # url of pages visited (promoted to site)
 
 
-else:
-    # for link in BeautifulSoup(html.read(), "lxml", parseOnlyThese=SoupStrainer('a')):
-    #     if link.has_attr('href'):
-    #         print(link['href'])
-    soup = BeautifulSoup(html.read(), "lxml")
+def checklinks(site):
+    global visitedPages
+    global pages
+    global pagesError
 
-    #links = soup.findAll("a")
+    try:
+        html = urlopen(site)
 
-    # for link in links:
-    #     if 'href' in link.attrs:
-    #         print(link.get_text())
-    #         print(link.attrs['href'])
-# .find("div", {"id":"content"})
+    except HTTPError as e:
+        print(e)
 
-    # != "#main-content" or link.attrs['href'] != "#navigation_drawer"
+    else:
+        visitedPages.add(site)
+        soup = BeautifulSoup(html.read(), "lxml")
 
-    for link in soup.findAll("a", href=True):
-#        if 'href' in link.attrs:
-        print(link.get_text())
-        if "http" in link.attrs['href']:
-            print(link.attrs['href'])
-            r = requests.get(link.attrs['href'])
-            print(r.status_code)
-        if  link.attrs['href'][0] == "/":
-            print(site + link.attrs['href'])
-            r = requests.get(site + link.attrs['href'][1:])
-            print(r.status_code)
+        links = soup.find("div", { "class" : "primary" }).findAll("a", href=True)
+        # numberlinks = len(links) # something that can be used for reporting later
+        # print(len(links))
+
+        for link in links:
+            page = link.get_text().strip(' \t\n\r')
+
+            if "http" in link.attrs['href']:
+                address = link.attrs['href']
+
+            # if internal link
+            elif  link.attrs['href'].startswith("/"):
+                address = site + link.attrs['href'][1:]
+
+            else:
+                address = link.attrs['href']
+
+
+            if address not in pages:
+                if address.startswith("mailto:"):
+                    status = "null"
+                else:
+                    r = requests.get(address)
+                    status = r.status_code
+                pages.add(address)
+                print(str(status) + " - " + page + " - " + address)
+                if r.status_code != requests.codes.ok:
+                    pagesError.add(address)
+                else:
+                    print("address: " + address)
+                    print(len(visitedPages))
+                    print("visited pages: " + str(visitedPages))
+                    print("Pages Error: " + str(len(pagesError)))
+                    if address not in visitedPages:
+                        if address.startswith("http://analytics.ncsu.edu/") and not address.endswith(".pdf") and not address.endswith(".jpg"):
+                            checklinks(address)
+
+checklinks(start)
+
 
 
 
